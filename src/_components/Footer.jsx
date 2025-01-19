46603da89e91d50cc571cb9776a1fae17a92f8fd
Kyle Whelan <kyle.aj.whelan@outlook.com>
2025-01-19 23:13:57 +0000

    added valid social links

--- src/_components/Footer.jsx
+++ src/_components/Footer.jsx
@@ -4,18 +4,21 @@ export default function Footer() {
       <a
         className="text-[#6d6d6d] hover:text-white duration-150"
         href="https://github.com/kyleajw"
+        target="_blank"
       >
         github
       </a>
       <a
         className="text-[#6d6d6d] hover:text-white duration-150"
-        href="https://github.com/kyleajw"
+        href="https://x.com/kyle_ajw"
+        target="_blank"
       >
         x.com
       </a>
       <a
         className="text-[#6d6d6d] hover:text-white duration-150"
-        href="https://github.com/kyleajw"
+        href="https://www.linkedin.com/in/kyle-whelan"
+        target="_blank"
       >
         linkedin
       </a>
